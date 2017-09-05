e55e2f126fef453e6b420e1df0202506c1300cf1
Daikon <daikon@daikon.localhost>
2017-09-05 09:44:12 +0700

    Fix code indentation

--- src/App.vue
+++ src/App.vue
@@ -3,99 +3,99 @@
     <nav class="navbar has-shadow">
       <div class="container">
         <div class="navbar-brand">
-        <router-link class="navbar-item" to="/dashboard">
-          KSRya
-        </router-link>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <b-field>
-              <b-input placeholder="Tìm..." type="search" icon="search" style="width: 350px">
-              </b-input>
-            </b-field>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              <span class="icon">
-                <b-icon icon="th-large"></b-icon>
-              </span>
-              <span>Tác vụ</span>
-            </a>
-            <div class="navbar-dropdown ">
-              <router-link class="navbar-item" to="/newimportsheet">
-                Nhập kho
-              </router-link>
-              <router-link class="navbar-item " to="/newexportsheet">
-                Xuất kho
-              </router-link>
-              <hr class="navbar-divider">
-              <a class="navbar-item ">
-                Thống kê
-              </a>
+          <router-link class="navbar-item" to="/dashboard">
+            KSRya
+          </router-link>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <b-field>
+                <b-input placeholder="Tìm..." type="search" icon="search" style="width: 350px">
+                </b-input>
+              </b-field>
             </div>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              <span class="icon">
-                <b-icon icon="suitcase"></b-icon>
-              </span>
-              <span>Quản lý</span>
-            </a>
-            <div class="navbar-dropdown ">
-              <router-link class="navbar-item" to='/inventory'>
-                Kho dược
-              </router-link>             
-              <router-link class="navbar-item" to='/productslist'>
-                Sản phẩm
-              </router-link>  
-              <a class="navbar-item ">
-                Danh mục
-              </a>
-              <a class="navbar-item ">
-                Nhà cung ứng
-              </a>
-              <a class="navbar-item ">
-                Phiếu nhập/xuất
-              </a>  
-              <hr class="navbar-divider">
-              <a class="navbar-item ">
-                Nhân viên
-              </a>
-            </div>
-          </div>
-
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              <span class="icon">
-                <b-icon icon="user-o"></b-icon>
-              </span>
-              <span>User</span>
-            </a>
-            <div class="navbar-dropdown ">
-              <a class="navbar-item">
-                Thông tin
-              </a>
-              <a class="navbar-item ">
-                Thiết lập
-              </a>
-              <hr class="navbar-divider">
-              <router-link class="navbar-item " to="/dbhelper">
-                DO NOT PRESS!!
-              </router-link>
-              <hr class="navbar-divider">
-              <a class="navbar-item ">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
                 <span class="icon">
-                  <b-icon icon="sign-out"></b-icon>
+                  <b-icon icon="th-large"></b-icon>
                 </span>
-                <span>Đăng xuất</span>
+                <span>Tác vụ</span>
               </a>
+              <div class="navbar-dropdown ">
+                <router-link class="navbar-item" to="/newimportsheet">
+                  Nhập kho
+                </router-link>
+                <router-link class="navbar-item " to="/newexportsheet">
+                  Xuất kho
+                </router-link>
+                <hr class="navbar-divider">
+                <a class="navbar-item ">
+                  Thống kê
+                </a>
+              </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                <span class="icon">
+                  <b-icon icon="suitcase"></b-icon>
+                </span>
+                <span>Quản lý</span>
+              </a>
+              <div class="navbar-dropdown ">
+                <router-link class="navbar-item" to='/inventory'>
+                  Kho dược
+                </router-link>
+                <router-link class="navbar-item" to='/productslist'>
+                  Sản phẩm
+                </router-link>
+                <a class="navbar-item ">
+                  Danh mục
+                </a>
+                <a class="navbar-item ">
+                  Nhà cung ứng
+                </a>
+                <a class="navbar-item ">
+                  Phiếu nhập/xuất
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item ">
+                  Nhân viên
+                </a>
+              </div>
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                <span class="icon">
+                  <b-icon icon="user-o"></b-icon>
+                </span>
+                <span>User</span>
+              </a>
+              <div class="navbar-dropdown ">
+                <a class="navbar-item">
+                  Thông tin
+                </a>
+                <a class="navbar-item ">
+                  Thiết lập
+                </a>
+                <hr class="navbar-divider">
+                <router-link class="navbar-item " to="/dbhelper">
+                  DO NOT PRESS!!
+                </router-link>
+                <hr class="navbar-divider">
+                <a class="navbar-item ">
+                  <span class="icon">
+                    <b-icon icon="sign-out"></b-icon>
+                  </span>
+                  <span>Đăng xuất</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    </nav>    
+    </nav>
     <div class="container is-fluid">
       <router-view style="margin-top: 80px"></router-view>
     </div>
