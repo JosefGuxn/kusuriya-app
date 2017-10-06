b50c823c033c444e693a41eece43094440239c9b
JosefGuxn <josefguxn@gmail.com>
2017-10-06 14:39:18 +0700

    Solve bug -1

--- src/components/DbHelper.vue
+++ src/components/DbHelper.vue
@@ -5,34 +5,20 @@
 </template>
 
 <script>
-import _ from 'lodash'
+// import _ from 'lodash'
 import {db} from '@/firebase'
 export default {
   firebase: {
-    exports: db.ref('exports'),
+    exports: db.ref('exports').orderByKey().startAt('1506995019000'),
     products: db.ref('inventory')
   },
   methods: {
     help () {
-      this.exports.forEach(e => {
-        var tmp = e.entries
-        var grandtotal = 0
-        if (tmp) {
-          Object.keys(tmp).forEach(i => {
-            grandtotal += tmp[i].total
-            var p = _.find(this.products, p => {
-              return p['.key'] === i
-            })
-            tmp[i].uom_retail = p.uom_retail
-            tmp[i].uom_wsale = p.uom_wsale
-            tmp[i].quan_w = tmp[i].is_wsale ? tmp[i].quantity : 0
-            tmp[i].quan_r = tmp[i].is_wsale ? 0 : tmp[i].quantity
-            tmp[i].retail_price = p.retail_price
-            tmp[i].wsale_price = p.wsale_price
-          })
-          this.$firebaseRefs.exports.child(e['.key']).child('entries').set(tmp)
-          this.$firebaseRefs.exports.child(e['.key']).child('grand_total').set(grandtotal)
-        } else {
+      this.products.forEach(p => {
+        if (p.remainder < 0) {
+          this.$firebaseRefs.products.child(p['.key']).child('quantity').set(parseInt(p.quantity) + 1)
+          this.$firebaseRefs.products.child(p['.key']).child('remainder').set(0)
+          console.log(p.product_name)
         }
       })
       console.log('You\'re welcome!')
