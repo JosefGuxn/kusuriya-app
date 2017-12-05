c46314798a18d50d735082a047a655fb97ba766a
JosefGuxn <josefguxn@gmail.com>
2017-12-05 14:07:46 +0700

    Switch database

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import * as firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-  /* apiKey: 'AIzaSyD7zmr42b5YHx6gAibxGNrQIqW8VN_Thfk',
+  apiKey: 'AIzaSyD7zmr42b5YHx6gAibxGNrQIqW8VN_Thfk',
   authDomain: 'nhathuoc41-9ecce.firebaseio.com',
   databaseURL: 'https://nhathuoc41-9ecce.firebaseio.com/',
   projectId: 'nhathuoc41-9ecce',
-  storageBucket: '' */
-  apiKey: 'AIzaSyCJ4QjPzX_Y5rloyMC2NyEQGUyL2yJQu5Y',
+  storageBucket: ''
+  /* apiKey: 'AIzaSyCJ4QjPzX_Y5rloyMC2NyEQGUyL2yJQu5Y',
   authDomain: 'kusuriya-13bcc.firebaseio.com/',
   databaseURL: 'https://kusuriya-13bcc.firebaseio.com/',
   projectId: 'kusuriya-13bcc',
-  storageBucket: ''
+  storageBucket: '' */
 })
 
 export const db = firebaseApp.database()
